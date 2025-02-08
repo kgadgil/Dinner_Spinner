ba7fc86da5d2dfdd0083c4ccd8e91614a478ea7b
kg <krightangle@gmail.com>
2025-02-08 15:24:22 -0500

    random breakfast flash

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is dinner spinner app. It makes decisions easier for what to make for breakfast, lunch, dinner or snacks. Press the button to generate a random value. 
+# Dinner Spinner
+
+This is dinner spinner app. It makes decisions easier for what to make for breakfast, lunch, dinner or snacks. 
+
+Press the button to generate a random value. 
--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, flash
+import json
 import pandas as pd
+import random
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+
+def read_json(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
 @app.route("/")
 def hello_world():
@@ -9,8 +16,10 @@ def hello_world():
 
 @app.route("/breakfast")
 def breakfast():
-    breakie = pd.read_csv("data/breakfast.csv")
-    return breakie.to_dict()
+    data = read_json("data/breakfast.csv")
+    choice = random.choice(list(data.keys()))
+    flash(f"You should have {choice} for breakfast")
+    return render_template("breakfast.html", data=data)
 
 @app.route("/dinner")
 def dinner():
